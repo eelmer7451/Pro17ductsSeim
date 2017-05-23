a4255f4c1da256c55d2e4702db59c68f0d07c7ba
Yul Etxabe <yuletxabe.dev@gmail.com>
2017-05-23 18:40:26 +0200

    Facturacion

--- ProyectoProductosC/ProyectoProductosC/Facturacion.c
+++ ProyectoProductosC/ProyectoProductosC/Facturacion.c
@@ -2,7 +2,7 @@
 
 void fCabeceraFactura()
 {
-	GoToXY(0, 0);
+	system("cls");
 	printf("\n%43sNombre: \n", "");
 	printf("%43sDomicilio: \n", "");
 	printf("%43sC.P.: \n%50s\n", "","");
@@ -27,8 +27,8 @@ void fAgregarFactura()
 	PEDIDO regPedido;
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
-	int nCliente;
-	int tamArchivoPedidos, sw=1,numLineas=0;
+	float total=0;
+	int tamArchivoPedidos, sw=1,numLineas=0,nCliente;
 	cpf = fopen(RUTAFICHEROCLIENTES, "rb");
 	if (cpf == NULL)
 	{
@@ -65,32 +65,66 @@ void fAgregarFactura()
 	}
 	regFactura.nFactura = fCalcularTamanoFicheroResumen(rpf);
 	tamArchivoPedidos = fCalcularTamanoFicheroPedidos(ppf);
+	fread(&regPedido, sizeof(regPedido), 1, ppf);
+	fCabeceraFactura();
+	nCliente = regPedido.nCliente;
 	while (!feof(ppf))
 	{
-		fread(&regPedido, sizeof(regPedido), 1, ppf);
-		fCabeceraFactura();
-		nCliente = regPedido.nCliente;
-		fseek(cpf, nCliente * sizeof(regCliente), SEEK_SET);
-		fread(&regCliente, sizeof(regCliente), 1, cpf);
-		GoToXY(53, 1);
-		printf("%s", regCliente.nombre);
-		GoToXY(53, 2);
-		printf("%s", regCliente.domicilio);
-		GoToXY(48, 3);
-		printf("%s  Municipio: %s", regCliente.codigoPostal,regCliente.municipio);
-		GoToXY(7, 5);
-		printf("%d-%d-%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900); 
-		GoToXY(55,5);
-		printf("%d", regFactura.nFactura);
-		GoToXY(0, numLineas+8);
-		//while (regPedido.nCliente == nCliente && !feof(ppf))
-		//{
+		if (nCliente == regPedido.nCliente)
+		{
+			fseek(cpf, nCliente * sizeof(regCliente), SEEK_SET);
+			fread(&regCliente, sizeof(regCliente), 1, cpf);
+			GoToXY(53, 1);
+			printf("%s", regCliente.nombre);
+			GoToXY(53, 2);
+			printf("%s", regCliente.domicilio);
+			GoToXY(48, 3);
+			printf("%s  Municipio: %s", regCliente.codigoPostal, regCliente.municipio);
+			GoToXY(7, 5);
+			printf("%d-%d-%d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
+			GoToXY(55, 5);
+			printf("%d", regFactura.nFactura);
+			GoToXY(0, numLineas + 8);
 			fseek(apf, sizeof(regArticulo)*regPedido.nArticulo, SEEK_SET);
 			fread(&regArticulo, sizeof(regArticulo), 1, apf);
 			printf("%12d  %23s  %8.2f  %12d  %9.2f\n", regPedido.nArticulo, regArticulo.denominacion, regPedido.pvp, regPedido.cantidad, regPedido.cantidad*regPedido.pvp);
-		//}
+			total += regPedido.cantidad*regPedido.pvp;
 			numLineas++;
+			fread(&regPedido, sizeof(regPedido), 1, ppf);
+		}
+		else
+		{
+			regFactura.iva = total - total*0.79;
+			regFactura.baseImponible = total*0.79;
+			regFactura.nCliente = nCliente;
+			fwrite(&regFactura, sizeof(regFactura), 1, rpf);
+			GoToXY(49, numLineas + 9);
+			printf("Base Imp.:   %.2f\n", total*0.79);
+			GoToXY(49, numLineas + 10);
+			printf("IVA:         %.2f\n", total - total*0.79);
+			GoToXY(49, numLineas + 11);
+			printf("TOTAL:       %.2f\n", total);
+			getch();
+			fwrite(&regFactura, sizeof(regFactura), 1, rpf);
+			fseek(ppf, sizeof(regPedido)*numLineas, SEEK_SET);
+			fread(&regPedido, sizeof(regPedido), 1, ppf);
+			nCliente = regPedido.nCliente;
+			numLineas = 0;
+			total = 0;
+			system("cls");
+			fCabeceraFactura();
+		}
 	}
+	regFactura.iva = total - total*0.79;
+	regFactura.baseImponible = total*0.79;
+	regFactura.nCliente = nCliente;
+	fwrite(&regFactura, sizeof(regFactura), 1, rpf);
+	GoToXY(49, numLineas + 9);
+	printf("Base Imp.:   %.2f\n", total*0.79);
+	GoToXY(49, numLineas + 10);
+	printf("IVA:         %.2f\n", total - total*0.79);
+	GoToXY(49, numLineas + 11);
+	printf("TOTAL:       %.2f\n", total);
 	getch();
 	fclose(cpf);
 	fclose(apf);
